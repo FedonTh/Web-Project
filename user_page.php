792f7053a919b0d42c5200a9f464ea071019c35c
Fedon Theodoropoulos <phaidontheodoropoulos@gmail.com>
2026-08-08 19:46:10 +0300

    Add files via upload

--- user_page.php
+++ user_page.php
@@ -1,25 +1,70 @@
 <?php
-
 session_start();
-if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="el">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Σελίδα Καταναλωτή</title>
+
+    <title>UniBite - Καταναλωτής</title>
+
     <link rel="stylesheet" href="style.css">
 </head>
-<body style="background: #fff;">
-    <div class="box">
-        <h1>Καλώς Ήρθες, <span><?= $_SESSION['username']; ?></span></h1>
-        <p>Αυτή είναι η σελίδα <span>καταναλωτή</span></p>
-        <button onclick="window.location.href='logout.php'"> Logout </button>
+
+<body>
+
+    <!-- Logo -->
+    <div class="logo">
+        UniBite
     </div>
+
+
+    <!-- Navbar -->
+    <ul class="navbar">
+
+        <li>
+            <a class="active" href="user_page.php">
+                Λίστα Αγγελιών
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                Χάρτης
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                Βαθμολογία
+            </a>
+        </li>
+
+    </ul>
+
+
+    <!-- Welcome message + Logout -->
+    <div class="user-header">
+
+        <div>
+            <h1>
+                Καλώς Ήρθες,
+                <span><?= $_SESSION['username']; ?></span>!
+            </h1>
+
+        </div>
+
+        <button onclick="window.location.href='logout.php'">
+            Logout
+        </button>
+
+    </div>
+
 </body>
+
 </html>
+
+
